877b29b0cb33818355ba99ce3ce9cbe2918a0bb5
YusufSyam <muhyusufsyamsyam@gmail.com>
2024-05-05 12:55:46 +0800

    Fitur search

--- src/pages/home/Catalog/Catalog.section.tsx
+++ src/pages/home/Catalog/Catalog.section.tsx
@@ -1,24 +1,46 @@
-import { Grid, Group, Pagination, Stack, Text } from "@mantine/core";
+import { Grid, Group, Pagination, Stack, Text, TextInput } from "@mantine/core";
 import React, { useEffect, useState } from "react";
 import CatalogItem, { ICatalogItem } from "./CatalogItem.component";
 import { dummyCatalogList } from "../../../utils/const/dummy";
+import { useDebouncedValue } from "@mantine/hooks";
+import { SearchFilled } from "../../../assets/icons/Fluent";
 
 export interface ICatalog {}
 
 const Catalog: React.FC<ICatalog> = ({}) => {
-  const catalogList: Array<ICatalogItem> = dummyCatalogList;
+  const [catalogList, setCatalogList] =
+    useState<Array<ICatalogItem>>(dummyCatalogList);
+
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [query] = useDebouncedValue(searchTerm, 500);
 
   const [activePage, setActivePage] = useState<number>(1);
   const [pageAmt, setPageAmt] = useState(0);
   const dataPerPageAmt = 8;
 
+  function handleSearchChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    setSearchTerm(e.target.value);
+    setActivePage(1);
+  }
+
   useEffect(() => {
     setPageAmt(Math.round(catalogList?.length / dataPerPageAmt + 0.4));
   }, [catalogList]);
 
+  useEffect(() => {
+    const tempCatalogList = dummyCatalogList?.filter((d: ICatalogItem) =>
+      d?.label?.toLowerCase()?.includes(query?.toLowerCase())
+    );
+
+    setCatalogList(tempCatalogList);
+  }, [query]);
+
+  console.log(query)
+  console.log(catalogList)
+
   return (
     <Stack>
-      <Group className="mb-4">
+      <Group className="mb-4 justify-between">
         <Stack className="gap-0">
           <Group>
             <Text className="text-primary-text font-poppins-semibold text-[32px] text-start">
@@ -28,13 +50,23 @@ const Catalog: React.FC<ICatalog> = ({}) => {
             <div className="w-1 h-1 mt-1 rounded-full bg-secondary-text"></div>
 
             <Group className="">
-              <Text className="text-secondary-text font-semibold">{dummyCatalogList?.length} Item</Text>
+              <Text className="text-secondary-text font-semibold">
+                Total {dummyCatalogList?.length} Item
+              </Text>
             </Group>
           </Group>
           <Text className="text-secondary-text text-start -mt-1">
             Jelajahi daftar barang dan status ketersediaannya
           </Text>
         </Stack>
+        <Group>
+          <TextInput
+            icon={<SearchFilled color="#dfdfdf" />}
+            onChange={handleSearchChange}
+            placeholder="Cari barang . . ."
+            size="md"
+          />
+        </Group>
       </Group>
       <Grid className="" gutter={32}>
         {catalogList
